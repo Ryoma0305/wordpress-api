7964fe0019f9bd97bdb2a79ac078ce8b6b26a2d6
Ryoma0305 <ryomaaa0602@gmail.com>
2022-11-28 13:40:53 +0900

    デプロイボタン追加

--- functions.php
+++ functions.php
@@ -35,3 +35,27 @@ function savepost_ogimage($post_ID) {
     file_get_contents($url);
 }
 
+// 管理画面にCloudflare Pagesのデプロイボタンを設置
+
+add_action( 'admin_menu', 'register_cf_deploy_menu_page' );
+function register_cf_deploy_menu_page(){
+    add_menu_page( 'デプロイ実行', 'デプロイ実行',
+    'manage_options', 'cf_deploy', 'cf_deploy_page', '', 1000 );
+}
+
+function cf_deploy_page(){
+    echo "<h2>Cloudflare Pagesへデプロイ</h2>";
+    $curl=curl_init("https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/e0cd0e84-bdde-4be3-830b-2693ee76d607");
+    curl_setopt($curl,CURLOPT_POST, TRUE);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($POST_DATA));
+    curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl,CURLOPT_COOKIEJAR,      'cookie');
+    curl_setopt($curl,CURLOPT_COOKIEFILE,     'tmp');
+    curl_setopt($curl,CURLOPT_FOLLOWLOCATION, TRUE);
+
+    $output= curl_exec($curl);
+    echo "デプロイフックを送信しました。";
+}
+
